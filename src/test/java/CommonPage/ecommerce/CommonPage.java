a7560ed57baf8e994bc50cc5491769ae534ef2b8
ravishangar1978 <54739612+ravishangar1978@users.noreply.github.com>
2021-01-10 16:45:58 -0600

    added commit in common page

--- src/test/java/CommonPage/ecommerce/CommonPage.java
+++ src/test/java/CommonPage/ecommerce/CommonPage.java
@@ -3,7 +3,7 @@ package CommonPage.ecommerce;
 import org.openqa.selenium.WebDriver;
 
 public class CommonPage {
-
+ // adding driver
 	WebDriver driver;
 	public CommonPage(WebDriver tmpdriver)
 	{
