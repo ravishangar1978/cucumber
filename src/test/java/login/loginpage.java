979fd0710fdb0c38e312e7b8748efc1e410d63cf
ravishangar1978 <54739612+ravishangar1978@users.noreply.github.com>
2021-06-05 12:09:44 -0500

    i added a new comment

--- src/test/java/login/loginpage.java
+++ src/test/java/login/loginpage.java
@@ -20,6 +20,7 @@ public class loginpage {
      
 	public WebElement loginObject() {
 				
+		// my first comment
 		return wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[6]/div/div/div/div/div[2]/div[1]/div/form/div[3]/input")));
 	}
 	
